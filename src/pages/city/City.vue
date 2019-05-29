731fbe43a27a921fb1a67a76f228d3c77bf910ca
q296009578 <296009578@qq.com>
2019-05-29 17:26:11 +0800

    city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './compoments/Header'
 import CitySearch from './compoments/Search'
+import CityList from './compoments/List'
+import CityAlphabet from './compoments/Alphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
