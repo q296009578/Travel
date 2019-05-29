5a091f14d37c86654b1e7cb36d552247e17de845
q296009578 <296009578@qq.com>
2019-05-29 18:51:26 +0800

    perfomance

--- src/pages/city/compoments/List.vue
+++ src/pages/city/compoments/List.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, key) of cities" :key="key">
+            <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
@@ -36,7 +36,17 @@ export default {
   name: 'List',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        // Bscrool提供的方法，滚动到某一个元素上
+        this.scroll.scrollToElement(element)
+      }
+    }
   },
   mounted () {
     this.scroll = new Bscrool(this.$refs.wrapper)
