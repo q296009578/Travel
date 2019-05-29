61343682cefc4d06b005e51ff7bb9383a455c17c
q296009578 <296009578@qq.com>
2019-05-29 14:39:35 +0800

    city-router

--- src/pages/home/compoments/Header.vue
+++ src/pages/home/compoments/Header.vue
@@ -2,10 +2,12 @@
     <div class="header">
         <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
         <div class="header-input"><span class="iconfont">&#xe632;</span></div>
-        <div class="header-right">
-            {{this.city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{this.city}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 
@@ -45,6 +47,7 @@ export default {
             width 1.24rem
             float right
             text-align center
+            color #fff
             .arrow-icon
                 margin-left -.04rem
                 font-size .24rem
