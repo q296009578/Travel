5bd37b68fc0b8c3f9c71fe13b4c3a917967885ec
q296009578 <296009578@qq.com>
2019-05-30 20:20:47 +0800

    detail list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,45 @@
     <div>
         <detail-banner></detail-banner>
         <detail-header></detail-header>
-        <div class="content"></div>
+        <div class="content">
+          <detail-list :list="list"></detail-list>
+        </div>
     </div>
 </template>
 
 <script>
 import DetailBanner from './compoments/Banner'
 import DetailHeader from './compoments/Header'
+import DetailList from './compoments/list'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人三馆组合票'
+          }, {
+            title: '成人三馆组合票'
+          }]
+        }, {
+          title: '成人五馆联票'
+        }]
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }, {
+        title: '学生票'
+      }]
+    }
   }
 }
 </script>
