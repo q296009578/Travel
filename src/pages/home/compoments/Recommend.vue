050f53ed2e3d8fa1a87b95ec20e5fb0fdeb4904a
q296009578 <296009578@qq.com>
2019-05-30 18:47:25 +0800

    banner finish

--- src/pages/home/compoments/Recommend.vue
+++ src/pages/home/compoments/Recommend.vue
@@ -2,14 +2,14 @@
     <div>
         <div class="title">推荐</div>
         <ul>
-            <li v-for="item of list" :key="item.id" class="item border-bottom">
+            <router-link  :to="'/detail/' + item.id" tag="li" v-for="item of list" :key="item.id" class="item border-bottom">
                 <img :src="item.imgUrl" alt="" class="item-img">
                 <div class="item-info">
                     <p class="item-title">{{item.title}}</p>
                     <p class="item-desc">{{item.desc}}</p>
                     <button class="item-button">查看详情</button>
                 </div>
-            </li>
+            </router-link>
         </ul>
     </div>
 </template>
